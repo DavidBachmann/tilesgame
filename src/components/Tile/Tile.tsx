c9c2e630583df59675b97e2bb237f8e4e7198c91
DavidBachmann <dabbi@dabbi.is>
2022-11-10 21:43:09 +0000

    chore: cleanup

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -69,12 +69,6 @@ export const Tile = ({
               set(false);
             }
           },
-          // config: {
-          //   mass: 1,
-          //   tension: 1,
-          //   friction: 100,
-          //   damping: 100,
-          // },
         };
       });
     },
