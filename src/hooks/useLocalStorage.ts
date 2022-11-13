0b2526c5de00e40c521b63c12c758f528620c007
DavidBachmann <dabbi@dabbi.is>
2022-11-13 16:10:59 +0000

    fix: switch out useEventCallback

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -7,22 +7,20 @@ import {
   SetStateAction,
   useCallback,
   useEffect,
-  useLayoutEffect,
   useRef,
   useState,
 } from "react";
 import { useEventListener } from "./useEventListener";
 
-function useEventCallback<Args extends unknown[], R>(fn: (...args: Args) => R) {
-  const ref = useRef<typeof fn>(() => {
-    throw new Error("Cannot call an event handler while rendering.");
-  });
-
-  useLayoutEffect(() => {
-    ref.current = fn;
-  }, [fn]);
-
-  return useCallback((...args: Args) => ref.current(...args), [ref]);
+function useStableCallback<Args extends any[], T>(
+  callback: (...args: Args[]) => T
+) {
+  const callbackContainer = useRef(callback);
+  callbackContainer.current = callback;
+  return useCallback(
+    (...args: Args) => callbackContainer.current(...args),
+    [callbackContainer]
+  );
 }
 
 declare global {
@@ -69,7 +67,7 @@ export function useLocalStorage<T>(
 
   // ... persists the new value to localStorage.
 
-  const setValue: SetValue<T> = useEventCallback((value) => {
+  const setValue: SetValue<T> = useStableCallback((value) => {
     // Prevent build error "window is undefined" but keeps working
 
     if (typeof window === "undefined") {
