55a146a0104da195fbae8fbbf96eb0f8da407ccc
DavidBachmann <dabbi@dabbi.is>
2022-11-19 07:57:20 +0000

    feat: redo useCountdown for Timer

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -1,7 +1,6 @@
 import { useMotionValue, useTransform } from "framer-motion";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useStore } from "../../StoreCreator";
-import { useCountdown } from "../../hooks/useCountdown";
 import { delay } from "../../utils";
 import { CONSTANTS } from "../../constants";
 import * as css from "./Timer.css";
@@ -12,18 +11,60 @@ const MS = 400;
 const COLOR_FROM = "rgb(229, 45, 34)";
 const COLOR_TO = "rgb(138, 201, 38)";
 
+const useCountdown = ({
+  initialValue,
+  onUpdate = () => {},
+  onComplete = () => {},
+}: {
+  initialValue: number;
+  onUpdate?: (value: number) => void;
+  onComplete?: () => void;
+}) => {
+  const [val, set] = useState(initialValue);
+  const [running, run] = useState(false);
+
+  const startCountdown = () => run(true);
+  const stopCountdown = () => run(false);
+
+  useEffect(() => {
+    const i = setInterval(() => {
+      if (!running) {
+        return;
+      }
+
+      return set((prev) => {
+        const newValue = Math.max(MIN, prev - 100);
+        onUpdate(newValue);
+        return newValue;
+      });
+    }, 100);
+
+    return () => clearInterval(i);
+  }, [val, running]);
+
+  useEffect(() => {
+    if (val <= 0) {
+      onComplete();
+    }
+  });
+
+  return {
+    setValue: set,
+    startCountdown,
+    stopCountdown,
+  };
+};
+
 export function Timer() {
   const game = useStore((state) => state.game);
   const setGameStatus = useStore((state) => state.actions.set_game_status);
   const setTimer = useStore((state) => state.actions.set_timer);
   const timer = useStore((state) => state.timer.count);
   const m = useMotionValue(MAX);
-
-  const { setCountdownToValue } = useCountdown({
-    countStart: MAX,
-    intervalMs: MS,
-    onUpdate: (time: number) => {
-      setTimer(time);
+  const { startCountdown, stopCountdown, setValue } = useCountdown({
+    initialValue: MAX,
+    onUpdate: (latestTime) => {
+      setTimer(latestTime);
     },
     onComplete: async () => {
       await delay(MS * 2);
@@ -32,10 +73,18 @@ export function Timer() {
   });
 
   useEffect(() => {
-    if (game.status === "in-progress" && timer > 0) {
-      setCountdownToValue(Math.min(timer, MAX));
+    if (game.status === "in-progress") {
+      startCountdown();
+    } else {
+      stopCountdown();
     }
-  }, [timer, game]);
+  }, [game.status]);
+
+  useEffect(() => {
+    if (timer) {
+      setValue(timer);
+    }
+  }, [timer, setValue]);
 
   const val = useTransform(m, [MIN, MAX], ["scaleX(0%)", "scaleX(100%)"]);
   const color = useTransform(m, [MIN, MAX], [COLOR_FROM, COLOR_TO]);
@@ -45,7 +94,14 @@ export function Timer() {
   return (
     <css.root>
       <css.track>
-        <css.meter style={{ transform: val, backgroundColor: color }} />
+        <css.meter
+          style={{
+            transform: val,
+            backgroundColor: color,
+            transitionDuration: "350ms",
+            transitionTimingFunction: "ease-out",
+          }}
+        />
       </css.track>
     </css.root>
   );
--- src/hooks/useToggle.ts
+++
@@ -1,9 +0,0 @@
-import { useCallback, useState } from "react";
-
-export function useToggle(defaultValue?: boolean): [boolean, () => void] {
-  const [value, setValue] = useState(!!defaultValue);
-
-  const toggle = useCallback(() => setValue((x) => !x), []);
-
-  return [value, toggle];
-}
