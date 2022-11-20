72dd4af0c7b5080ac727513af8f38ce374a597d6
DavidBachmann <dabbi@dabbi.is>
2022-11-20 21:54:10 +0000

    chore: add meta stuff

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,24 @@ import { ConfigProvider } from "../src/context/ConfigContext";
 import { StoreCreator } from "../src/StoreCreator";
 import { PlayerProvider } from "../src/context/PlayerContext";
 import "../styles/globals.css";
+import Head from "next/head";
 
 const UI = dynamic(() => import("../src/components/UI"), { ssr: false });
 
 export default function App({ Component }: AppProps) {
   return (
     <ConfigProvider>
+      <Head>
+        <title>Tiles Game</title>
+        <meta property="og:title" content="Tiles game" key="title" />
+        <meta
+          property="og:description"
+          content="Play tiles online, for free!"
+          key="description"
+        />
+
+        <link rel="manifest" href="site.webmanifest" />
+      </Head>
       <StoreCreator>
         <PlayerProvider>
           <UI>
